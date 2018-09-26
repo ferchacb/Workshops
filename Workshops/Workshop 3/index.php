ba24bd5ba4efdb1ce981b600e92071bdbc2b43e8
ferchacb <mamurilloa@est.utn.ac.cr>
2018-09-25 20:40:59 -0600

    Workshop 3

---
+++ Workshops/Workshop 3/index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $paises = array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid", "Sweden"=>"Stockholm", "United Kingdom"=>"London", "Cyprus"=>"Nicosia", "Lithuania"=>"Vilnius", "Czech Republic"=>"Prague", "Estonia"=>"Tallin", "Hungary"=>"Budapest", "Latvia"=>"Riga", "Malta"=>"Valetta", "Austria" => "Vienna", "Poland"=>"Warsaw") ;
+    ksort($paises);
+  
+    $temperaturas = array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
+ 
+    $temperaturas2 = array(78, 60, 62, 68, 71, 68, 73, 85, 66, 64, 76, 63, 75, 76, 73, 68, 62, 73, 72, 65, 74, 62, 62, 65, 64, 68, 73, 75, 79, 73);
+    $total = array_sum($temperaturas) / count($temperaturas);
+
+    sort($temperaturas);
+    $hightest = array_slice($temperaturas, 0, 7); 
+
+
+    rsort($temperaturas2);
+    $lowest = array_slice($temperaturas2, 0, 7);
+    require "indexhtml.php";
+?>
---
+++ Workshops/Workshop 3/indexhtml.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ordenamiento</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <div class="container">
+    <h2>Paises&Capitales</h2>
+    <br>
+        <?php foreach($paises as $y=>$y_value):?>
+            
+            <h6>La Capital de <?php echo $y;?> es: <?php echo $y_value;?></h6>
+            <br>
+        <?php endforeach;?>
+        <h2>Rango de temperaturas</h2><br>
+        <h6>La temperatura promedio es : <?php echo $total;?></h6><br>
+        <h6>List of seven lowest temperatures : <?php echo $hightest[0].",",$hightest[2].",",$hightest[3].",",$hightest[4].",",$hightest[5]?></h6><br>
+        <h6>List of seven highest temperatures : <?php echo $lowest[0].",",$lowest[2].",",$lowest[3].",",$lowest[4].",",$lowest[5];?></h6><br>
+    </div>
+
+
+</body>
+</html>
